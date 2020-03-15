e8ce78059359b92136963f71872c304f416d398f
DCRSgk <npezcrs@outlook.com>
2020-03-15 19:39:37 +0800

    Revert "0.0"
    This reverts commit 682560cf8de6612fd7011e5ed28137787ee47b4a.

--- 基础编程题目集/test/源.cpp
+++ 基础编程题目集/test/源.cpp
@@ -1,13 +1,8 @@
 #include<iostream>
 #include<iomanip>    //setiosflags(ios::fixed) << setprecision(2)
 #include<cstdio>
-#include<sstream>
 using namespace std;
+int main() {
 
-#include<stdio.h>
-    int main() {
-        int fahr = 150, celsius = 0;
-        celsius = 5 * (fahr - 32);
-        printf("%d", celsius);
-        return 0;
-    }
+    return 0;
+}
