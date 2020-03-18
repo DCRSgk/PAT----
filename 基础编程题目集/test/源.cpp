8fa2eabd75b5336431e9808d6021f4b476fa8c33
13275991036 <npezcrs@126.com>
2020-03-18 19:45:56 +0800

    20200318

--- 基础编程题目集/test/源.cpp
+++ 基础编程题目集/test/源.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 #include<iomanip>    //setiosflags(ios::fixed) << setprecision(2)
 #include<cstdio>
+#define sum 4+3
 using namespace std;
 int main() {
-
+    int i = sum;
+    cout << 2 * i << endl;
     return 0;
 }
